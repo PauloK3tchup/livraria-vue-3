373660f83f7b796d69d1df6c869119d6b9858e5a
PauloK3tchup <passandoalimpo13@gmail.com>
2023-04-28 15:15:25 -0300

    Adição de peppinos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,59 @@
+<script>
+import PeppinoGif from "../src/components/PeppinoGif.vue";
+
+export default {
+  components: {
+    PeppinoGif,
+  },
+};
+</script>
 <template>
   <header>
-    <router-link :to="{ name: 'home' }">Home</router-link>
-    <span> | </span>
-    <router-link to="/categorias">Categorias</router-link>
-    <span> | </span>
-    <router-link to="/editoras">Editoras</router-link>
-    <span> | </span>
-    <router-link to="/autores">Autores</router-link>
-    <span> | </span>
-    <router-link to="/livros">Livros</router-link>
+    <router-link class="btn-nav" :to="{ name: 'home' }">Home</router-link>
+    <router-link class="btn-nav" to="/categorias">Categorias</router-link>
+    <router-link class="btn-nav" to="/editoras">Editoras</router-link>
+    <router-link class="btn-nav" to="/autores">Autores</router-link>
+    <router-link class="btn-nav" to="/livros">Livros</router-link>
   </header>
   <main>
     <RouterView />
   </main>
+  <PeppinoGif
+    :fotoclick="'https://static.wikia.nocookie.net/pizzatower/images/8/86/Do_do_do_do_dah...yeah.gif'"
+  />
+  <PeppinoGif
+    :fotoclick="'https://static.wikia.nocookie.net/pizzatower/images/2/29/Peppinognomecutscene.gif/'"
+  />
+  <PeppinoGif
+    :fotoclick="'https://static.wikia.nocookie.net/pizzatower/images/e/e4/Peppino_%28Handsome_Taunt%29.png/'"
+  />
+  <PeppinoGif
+    :fotoclick="'https://static.wikia.nocookie.net/pizzatower/images/e/e4/Peppino_%28Handsome_Taunt%29.png/'"
+  />
 </template>
+<style scoped>
+.btn-nav {
+  text-decoration: none;
+  color: rgb(189, 0, 0);
+  font-size: 20px;
+  background-color: #000;
+  padding: 10px;
+  border-radius: 5px;
+  transition: 0.25s;
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.btn-nav:hover {
+  color: black;
+  background-color: rgb(189, 0, 0);
+  transform: scale(1.3);
+  z-index: 1;
+}
+
+.btn-nav:active {
+  color: black;
+  background-color: rgb(189, 0, 0);
+  transform: scale(0.9);
+}
+</style>
---
+++ src/components/PeppinoGif.vue
@@ -0,0 +1,79 @@
+<script>
+export default {
+  props: ["fotoclick"],
+};
+</script>
+
+<template>
+  <div class="peppino">
+    <img
+      class="peppino-default"
+      src="https://static.wikia.nocookie.net/pizzatower/images/d/d2/Idle_demo1.gif/"
+      alt="peppito"
+    />
+    <img
+      class="peppino-tchararam"
+      :src="fotoclick"
+      alt=""
+      id="tcharam"
+      onmousedown="document.getElementById('tcharam').src='{{ fotoclick }}';"
+    />
+    <img
+      onmouseover="document.getElementById('mamamia').src='https://static.wikia.nocookie.net/pizzatower/images/3/3f/OH_MAMA_MIA.gif/'"
+      class="peppino-mamamia"
+      src="https://static.wikia.nocookie.net/pizzatower/images/3/3f/OH_MAMA_MIA.gif/"
+      alt=""
+      id="mamamia"
+    />
+  </div>
+</template>
+<style scoped>
+.peppino {
+  display: inline-block;
+  width: 70px;
+  height: 70px;
+}
+
+.peppino-default {
+  width: 70px;
+  height: 70px;
+  display: block;
+}
+
+.peppino:hover .peppino-default {
+  width: 70px;
+  height: 70px;
+  display: none;
+}
+
+.peppino:hover .peppino-mamamia {
+  display: block;
+}
+
+.peppino:active .peppino-tchararam {
+  display: block;
+}
+
+.peppino:active .peppino-mamamia {
+  display: none;
+}
+
+.peppino:active .peppino-default {
+  display: none;
+}
+
+.peppino:focus .peppino-sensual {
+  display: block;
+}
+
+.peppino-tchararam {
+  width: 70px;
+  height: 70px;
+  display: none;
+}
+.peppino-mamamia {
+  width: 70px;
+  height: 70px;
+  display: none;
+}
+</style>
